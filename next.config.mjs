4feaeee5254c534b1e3c3691c5f84c86accbeb38
Ishaq Ibrahim <ishaq.ibrahim@convertdigital.com.au>
2024-09-05 00:31:29 +0500

    set up PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+import nextPWA from "next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+});
+
+export default withPWA(nextConfig);
